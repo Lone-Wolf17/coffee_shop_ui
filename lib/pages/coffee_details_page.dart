1818c513f0fd0982d93bf1193c309431403c070a
lone-wolf <adeyanju.akorede1@gmail.com>
2021-02-17 11:49:23 +0100

    ** Coffee Details Page UI Completed

--- lib/pages/coffee_details_page.dart
+++ lib/pages/coffee_details_page.dart
@@ -1,10 +1,140 @@
+import 'package:coffee_shop_ui/utils/constants.dart';
+import 'package:coffee_shop_ui/utils/static_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CoffeeDetailsPage extends StatelessWidget {
   static const String pageRoutePath = "/coffee-details";
 
+  Widget _doubleColText(String textOne, String textTwo) {
+    return Padding(padding: const EdgeInsets.symmetric(vertical: 12), child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          textOne,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kPrimaryColor
+          ),
+        ),
+        Text(
+          textTwo,
+          style: TextStyle(
+            color: kPrimaryColor
+          ),
+        )
+      ],
+    ),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+              left: 100,
+              child: Opacity(
+            opacity: 0.1,
+            child: Image.asset(
+              StaticData.coffee2ImgPath,
+              width: 150,
+            ),
+          )),
+          Positioned(
+            top: 0,
+              right: -180,
+              child: Image.asset(StaticData.squareImgPath)),
+          Positioned(
+            left: -70,
+              bottom: -40,
+              child: Image.asset(StaticData.drumImgPath)),
+          Positioned(
+            top: 0,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                alignment: Alignment.topCenter,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      StaticData.product1ImgPath
+                    )
+                  )
+                ),
+              )),
+          Positioned(
+              top: 280,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Iced and chilled Coffee",
+                      style: TextStyle(color: kTextColor),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Latte with Almond Milk",
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 22
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Almond milk is mostly water, so mixed with coffee, the almond flavor is totally overpowered by strong coffee. A good almond lattee needs to be made with a thick homemade vanilla almond milk creamer.",
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: kTextColor,
+                        height: 1.8,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Divider(),
+                    _doubleColText("Quantity", "4"),
+                    Divider(),
+                    _doubleColText("Amount Payable", "\$10.44 USD"),
+                    SizedBox(height: 36),
+                    Container(
+                      width: double.infinity,
+                      height: ScreenUtil().setHeight(48),
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        color: kTextColor,
+                        onPressed: (){},
+                        child: Text(
+                          "Add to my cart",
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
